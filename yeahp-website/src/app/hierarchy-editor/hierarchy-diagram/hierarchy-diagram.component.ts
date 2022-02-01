c525995adeb41442ff4fa21fb79e5710b7e23fb0
Frédéric LAUDARIN <frederic.laudarin@gmail.com>
2022-02-02 00:56:02 +0100

    Integration of SVG into the Angular component system

--- yeahp-website/src/app/hierarchy-editor/hierarchy-diagram/hierarchy-diagram.component.ts
+++ yeahp-website/src/app/hierarchy-editor/hierarchy-diagram/hierarchy-diagram.component.ts
@@ -2,14 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hierarchy-diagram',
-  templateUrl: './hierarchy-diagram.component.html',
-  styleUrls: ['./hierarchy-diagram.component.scss']
+  templateUrl: './hierarchy-diagram.component.svg',
+  styleUrls: ['./hierarchy-diagram.component.scss'],
 })
 export class HierarchyDiagramComponent implements OnInit {
+  public nodeTransform: string;
+  public nodeWidth: string;
+  public nodeHeight: string;
+  public nodeTextX: number;
+  public nodeTextY: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.nodeTransform = 'translate(50,400)';
+    this.nodeWidth = '150px';
+    this.nodeHeight = '40px';
+    this.nodeTextX = 10;
+    this.nodeTextY = 30;
   }
 
+  ngOnInit(): void {}
 }
