187b4c958bd113a4bef1a8016e87b51f2e40e712
Frédéric LAUDARIN <frederic.laudarin@gmail.com>
2022-02-02 01:16:05 +0100

    WIP: node presentation class

---
+++ yeahp-website/src/app/hierarchy-editor/hierarchy-diagram/hierarchy-node-presentation.ts
@@ -0,0 +1,12 @@
+class HierarchyNodePresentation {
+  public nodeTransform: string = '';
+  public nodeWidth: number = 0;
+  public nodeHeight: number = 0;
+  public nodeTextX: number = 0;
+  public nodeTextY: number = 0;
+  public title: string;
+
+  constructor(title: string) {
+    this.title = title;
+  }
+}
